5000b7067a9c86ea113abce3cc305174657a8c3c
Charlie Patterson <pattersoncharlesl@gmail.com>
2018-01-24 15:58:08 -0500

    Add login/logout functionality

--- blog/blog.py
+++ blog/blog.py
@@ -47,6 +47,76 @@ def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
+@application.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries=entries)
+
+@application.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?,?)',
+				[request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@application.route('/login', methods=['GET','POST'])
+def login():
+	error = None 
+	if request.methods == 'POST':
+		if request.form['username'] != application.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != application.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@application.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
