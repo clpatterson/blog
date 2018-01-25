6eb7912f7dfafc4b1e34348f3693c1d33dde9f89
Charlie Patterson <pattersoncharlesl@gmail.com>
2018-01-25 15:58:45 -0500

    Add css styling

--- blog/blog.py
+++ blog/blog.py
@@ -59,7 +59,7 @@ def add_entry():
 	if not session.get('logged_in'):
 		abort(401)
 	db = get_db()
-	db.execute('insert into entries (title, text) values (?,?)',
+	db.execute('insert into entries (title, text) values (?, ?)', 
 				[request.form['title'], request.form['text']])
 	db.commit()
 	flash('New entry was successfully posted')
@@ -68,7 +68,7 @@ def add_entry():
 @application.route('/login', methods=['GET','POST'])
 def login():
 	error = None 
-	if request.methods == 'POST':
+	if request.method == 'POST':
 		if request.form['username'] != application.config['USERNAME']:
 			error = 'Invalid username'
 		elif request.form['password'] != application.config['PASSWORD']:
